2a2b3935c16f3d5a8e8719ac267296f6b1fec49f
unknown <arizqiramadan@gmail.com>
2025-01-21 10:00:34 +0700

    first commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -130,7 +130,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="text-center">
-                        <p class="mb-0 text-muted">&copy; <script>document.write(new Date().getFullYear())</script> Velzon. Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand</p>
+                        <p class="mb-0 text-muted">&copy; <script>document.write(new Date().getFullYear())</script> Grenviro Monitoring. Crafted with <i class="mdi mdi-heart text-danger"></i> by Aiyothings</p>
                     </div>
                 </div>
             </div>
