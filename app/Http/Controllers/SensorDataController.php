c7e38a9ff9c2ccccf59de895bb167e7258b7a99f
unknown <arizqiramadan@gmail.com>
2025-01-22 07:55:05 +0700

    first commit

--- app/Http/Controllers/SensorDataController.php
+++ app/Http/Controllers/SensorDataController.php
@@ -68,10 +68,9 @@ class SensorDataController extends Controller
             $query->whereDate('timestamp', now());
         } 
         elseif ($filter == '1y') {
-            $query->where('timestamp', now()->subDay());
-        } 
-        elseif ($filter == '1w') {
-            $query->whereBetween('timestamp', now()->subDays(7));
+            $query->whereDate('timestamp', now()->subDay());
+        } elseif ($filter == '1w') {
+            $query->whereBetween('timestamp', [now()->subDays(7), now()]);
         } elseif ($filter == '1m') {
             $query->whereMonth('timestamp', $currentMonth)->whereYear('timestamp', $currentYear);
         } elseif ($filter == '6y') {
